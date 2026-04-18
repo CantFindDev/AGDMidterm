2dddb090b3fe727fb626333d32c52ecf86071f69
Çınar Güvençli <cinarguvencli@hotmail.com>
2026-04-18 06:51:40 +0300

    feat: added inputs and mapped the inputs to COAAvatar
    Inputs added:
    Turn -> Mouse-X
    LookUp -> Mouse-Y
    MoveForward -> W-S
    MoveRight -> A-D
    Jump -> Space
    Run -> Shift

--- ClashOfAvatars/Source/ClashOfAvatars/Private/COAAvatar.cpp
+++ ClashOfAvatars/Source/ClashOfAvatars/Private/COAAvatar.cpp
@@ -3,7 +3,10 @@
 
 #include "COAAvatar.h"
 
-ACOAAvatar::ACOAAvatar()
+#include "GameFramework/CharacterMovementComponent.h"
+
+ACOAAvatar::ACOAAvatar() :
+RunSpeed(300.0f)
 {
 	mSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	mSpringArm->TargetArmLength = 300.0f;
@@ -11,4 +14,58 @@ ACOAAvatar::ACOAAvatar()
 
 	mCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("MainCamera"));
 	mCamera->SetupAttachment(mSpringArm,USpringArmComponent::SocketName);
+
+	mCamera->bUsePawnControlRotation = false;
+	mSpringArm->bUsePawnControlRotation = true;
+	bUseControllerRotationYaw = false;
+}
+
+void ACOAAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn",this, &ACharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp",this, &ACharacter::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAxis("MoveForward",this,&ACOAAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight",this,&ACOAAvatar::MoveRight);
+
+	PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump",IE_Released,this,&ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAction("Run",IE_Pressed,this,&ACOAAvatar::RunPressed);
+	PlayerInputComponent->BindAction("Run",IE_Released,this,&ACOAAvatar::RunReleased);
+}
+
+
+void ACOAAvatar::RunPressed()
+{
+	GetCharacterMovement()->MaxWalkSpeed = RunSpeed;
+}
+
+void ACOAAvatar::RunReleased()
+{
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+}
+
+void ACOAAvatar::MoveForward(float value)
+{
+	AddMovementInput(GetMovmentDirectionBasedOnAxis(EAxis::X),value);
+}
+
+void ACOAAvatar::MoveRight(float value)
+{
+	AddMovementInput(GetMovmentDirectionBasedOnAxis(EAxis::Y),value);
+}
+
+FVector ACOAAvatar::GetMovmentDirectionBasedOnAxis(EAxis::Type dir)
+{
+	return FRotationMatrix(GetYaw()).GetUnitAxis(dir);
+}
+
+FRotator ACOAAvatar::GetYaw()
+{
+	FRotator rotation = GetController()->GetControlRotation();
+	FRotator yawRotation(0.0f,rotation.Yaw,0.0f);
+	return yawRotation;
 }
--- ClashOfAvatars/Source/ClashOfAvatars/Public/COAAvatar.h
+++ ClashOfAvatars/Source/ClashOfAvatars/Public/COAAvatar.h
@@ -20,9 +20,26 @@ public:
 
 	ACOAAvatar();
 
+	UPROPERTY(EditAnywhere,Category = "COA")
+	float RunSpeed;
+
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Character")
 	UCameraComponent* mCamera;
 
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Character")
 	USpringArmComponent* mSpringArm;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	void MoveForward(float value);
+	void MoveRight(float value);
+
+	void RunPressed();
+	void RunReleased();
+
+	//Helper methods :)
+	FRotator GetYaw();
+	FVector GetMovmentDirectionBasedOnAxis(EAxis::Type dir);
 };
