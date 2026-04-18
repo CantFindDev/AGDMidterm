edf7fa1deef06b1adcb41ea1d190db184ed0fd9d
Çınar Güvençli <cinarguvencli@hotmail.com>
2026-04-18 06:05:33 +0300

    feat: added COABaseCharacter and COAGameMode

--- ClashOfAvatars/Source/ClashOfAvatars/ClashOfAvatars.cpp
+++ ClashOfAvatars/Source/ClashOfAvatars/ClashOfAvatars.cpp
@@ -4,5 +4,3 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClashOfAvatars, "ClashOfAvatars" );
-
-DEFINE_LOG_CATEGORY(LogClashOfAvatars)
--- ClashOfAvatars/Source/ClashOfAvatarsEditor.Target.cs
+++ ClashOfAvatars/Source/ClashOfAvatarsEditor.Target.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class ClashOfAvatarsEditorTarget : TargetRules
 {
-	public ClashOfAvatarsEditorTarget(TargetInfo Target) : base(Target)
+	public ClashOfAvatarsEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
